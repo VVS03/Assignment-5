482d1ff348dcee0aa4f3918805683fc360509413
VVS03 <100142259+VVS03@users.noreply.github.com>
2022-06-20 19:47:28 +0530

    Add files via upload

--- Assignment_05/Q9.py
+++ Assignment_05/Q9.py
@@ -1,17 +1,31 @@
-input_string = input("Enter the string you want to check for : ")
+n= int(input("no of words:"))
+input_list = []
 
-def count_word_of_string(str):
+for i in range (0,n):
+    input_word = input("Enter word : ")
+    input_list.append(input_word)
+
+k= {}
+for i in input_list:
+        if i not in k:
+            k[i]=1
+        else :
+            k[i]+=1
+print ("no of occurance =",k)
+# input_string = input("Enter the string you want to check for : ")
+
+# def count_word_of_string(str):
     
-    word_count = dict()
+#     word_count = dict()
     
-    words = str.split()
+#     words = str.split()
 
-    for word in words:
-        if word in word_count:
-            word_count[word] += 1
-        else:
-            word_count[word] = 1
+#     for word in words:
+#         if word in word_count:
+#             word_count[word] += 1
+#         else:
+#             word_count[word] = 1
 
-    return word_count
+#     return word_count
 
-print(count_word_of_string(input_string))
+# print(count_word_of_string(input_string))
