af493b42134ca76c8e9a149e333d667689d0e031
VVS03 <100142259+VVS03@users.noreply.github.com>
2022-06-21 22:19:32 +0530

    Add files via upload

---
+++ Assignment_05/Q9.py
@@ -0,0 +1,18 @@
+
+input_string = input("Enter the string you want to check for : ")
+
+def count_word_of_string(str):
+    
+    word_count = dict()
+    
+    words = str.split()
+
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    return word_count
+
+print(count_word_of_string(input_string))
